c7078f5312679b2a0886f2beb790c91092b2226c
systopia <endres@systopia.de>
2015-09-03 14:00:51 +0200

    normalise API draft

--- api/v3/OsmLookup.php
+++ api/v3/OsmLookup.php
@@ -42,3 +42,27 @@ function civicrm_api3_osm_lookup_call($params) {
     return civicrm_api3_create_error('de.systopia.osm: geocoding failed');
   }
 }
+
+
+
+ /**
+  * This API call will try to normalise the given address
+  *
+  * @param street_address   address street and number 
+  * @param postal_code      address postal code
+  * @param city             address postal city
+  * @param country_id       address country ID (default is CiviCRM default country)
+  * 
+  * @return the params above if they could be normalised.
+  *         also, the key 'query' return the original values
+  *         'is_error', 'error_code', 'error_msg' is set upon an error
+  *         error_code  is    0 => no error
+  *                           1 => bad domain (currently this only works for Germany)
+  *                           2 => cannot parse (there's an issue with parsing street_address)
+  *                           3 => not enough information
+  */
+function civicrm_api3_osm_lookup_normalise($params) {
+  // TODO: implement
+  return civicrm_api3_create_success($params);
+}
+
