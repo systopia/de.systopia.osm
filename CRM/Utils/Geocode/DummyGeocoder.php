35bd9c7f32983a7d34db226055ac2b20398e0949
B. Endres <endres@systopia.de>
2021-11-22 09:14:53 +0100

    [#22] added dummy geodcoder

---
+++ CRM/Utils/Geocode/DummyGeocoder.php
@@ -0,0 +1,46 @@
+<?php
+/*
+  +--------------------------------------------------------------------+
+  | CiviCRM OSM Geocoding module (SYS-OSM)                             |
+  +--------------------------------------------------------------------+
+  | Copyright SYSTOPIA (c) 2021                                        |
+  +--------------------------------------------------------------------+
+  | This is free software; you can copy, modify, and distribute it     |
+  | under the terms of the GNU Affero General Public License           |
+  | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+  |                                                                    |
+  | SYS-OSM is distributed in the hope that it will be useful, but     |
+  | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+  | See the GNU Affero General Public License for more details.        |
+  |                                                                    |
+  | You should have received a copy of the GNU Affero General Public   |
+  | License and the SYS-OSM Licensing Exception along                  |
+  | with this program; if not, contact SYSTOPIA                        |
+  | at info[AT]systopia[DOT]de. If you have questions about the        |
+  | GNU Affero General Public License or the licensing of CiviCRM,     |
+  | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+  +--------------------------------------------------------------------+
+*/
+
+
+/**
+ * Class that represents a dummy geocoder, i.e. does nothing
+ */
+class CRM_Utils_Geocode_DummyGeocoder {
+
+  /**
+   * function that takes an address array and gets the latitude / longitude
+   * and postal code for this address. Note that at a later stage, we could
+   * make this function also clean up the address into a more valid format
+   *
+   * @param array $values associative array of address data: country, street_address, city, state_province, postal code
+   * @param boolean $stateName this params currently has no function
+   *
+   * @return boolean true if we modified the address, false otherwise
+   * @static
+   */
+  static function format(&$values, $stateName = FALSE) {
+    return true;
+  }
+}
