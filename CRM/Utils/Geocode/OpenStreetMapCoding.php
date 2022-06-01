d702555d689d4aca0ceb8fac036b286512e7a791
Seamus Lee <seamuslee001@gmail.com>
2022-06-01 11:12:14 +1000

    Update OSM provider to support Search Kit Proximinty search

--- CRM/Utils/Geocode/OpenStreetMapCoding.php
+++ CRM/Utils/Geocode/OpenStreetMapCoding.php
@@ -21,7 +21,7 @@
   | GNU Affero General Public License or the licensing of CiviCRM,     |
   | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
   +--------------------------------------------------------------------+
-*/
+ */
 
 /**
  *
@@ -33,13 +33,13 @@
 /**
  * Class that uses OpenStreetMap (OSM) API to retrieve the lat/long of an address
  *
- * This CiviCRM extension requests geodata from nominatim.osm.org, 
- * a service of OpenStreetMap Foundation. We have been advised that OSM servers 
- * might not be suitable for massive data requests, but so far could not 
- * get precise information on what this exactly means. Also, we don't know what 
- * the consequences of requests too massive for the OSM infrastructure might be. 
- * Therefore we recommend that you consider using the 'throtte' option for the 
- * "Geocode and Parse Adresses" cronjob if processing data sets containing more 
+ * This CiviCRM extension requests geodata from nominatim.osm.org,
+ * a service of OpenStreetMap Foundation. We have been advised that OSM servers
+ * might not be suitable for massive data requests, but so far could not
+ * get precise information on what this exactly means. Also, we don't know what
+ * the consequences of requests too massive for the OSM infrastructure might be.
+ * Therefore we recommend that you consider using the 'throtte' option for the
+ * "Geocode and Parse Adresses" cronjob if processing data sets containing more
  * than 10.000 addresses.
  */
 class CRM_Utils_Geocode_OpenStreetMapCoding {
@@ -50,7 +50,7 @@ class CRM_Utils_Geocode_OpenStreetMapCoding {
    * @var string
    * @static
    */
-  static protected $_server = 'nominatim.openstreetmap.org';
+  protected static $_server = 'nominatim.openstreetmap.org';
 
   /**
    * uri of service
@@ -58,7 +58,7 @@ class CRM_Utils_Geocode_OpenStreetMapCoding {
    * @var string
    * @static
    */
-  static protected $_uri = '/search';
+  protected static $_uri = '/search';
 
   /**
    * function that takes an address array and gets the latitude / longitude
@@ -71,10 +71,8 @@ class CRM_Utils_Geocode_OpenStreetMapCoding {
    * @return boolean true if we modified the address, false otherwise
    * @static
    */
-  static function format(&$values, $stateName = FALSE) {
+  public static function format(&$values, $stateName = FALSE) {
     CRM_Utils_System::checkPHPVersion(5, TRUE);
-
-    $config = CRM_Core_Config::singleton();
 
     $params = array();
 
@@ -135,6 +133,29 @@ class CRM_Utils_Geocode_OpenStreetMapCoding {
       $url .= '&' . urlencode($key) . '=' . urlencode($value);
     }
 
+    $coord = self::makeRequest($url);
+
+    $values['geo_code_1'] = $coord['geo_code_1'] ?? 'null';
+    $values['geo_code_2'] = $coord['geo_code_2'] ?? 'null';
+
+    if (isset($coord['geo_code_error'])) {
+      $values['geo_code_error'] = $coord['geo_code_error'];
+    }
+
+    return isset($coord['geo_code_1'], $coord['geo_code_2']);
+  }
+
+  public static function getCoordinates($address): array {
+    return self::makeRequest(urlencode($address));
+  }
+
+  /**
+   * @param string $add
+   *   Url-encoded address
+   * @return array
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  private static function makeRequest($add): array {
     // Nominatim requires that we cache lookups, since they're donating this
     // service for free.
     $cache = CRM_Utils_Cache::create(['type' => ['SqlGroup'], 'name' => 'geocode_osm']);
@@ -152,19 +173,20 @@ class CRM_Utils_Geocode_OpenStreetMapCoding {
       // everybody to do with the organisation along with an identifier for the
       // organisation could be sensitive.
       // @see https://operations.osmfoundation.org/policies/nominatim/
-      $appName = $config->geoAPIKey ?: substr(sha1(CRM_Core_BAO_Domain::getDomain()->name . CIVICRM_SITE_KEY), 0, 12);
-      $request = $client->request('GET', $url, ['headers' =>  ['User-Agent' => "CiviCRM instance ($appName)"]]);
+      $appName =  CRM_Core_Config::singleton()->geoAPIKey ?: substr(sha1(CRM_Core_BAO_Domain::getDomain()->name . CIVICRM_SITE_KEY), 0, 12);
+      $request = $client->request('GET', $url, ['headers' => ['User-Agent' => "CiviCRM instance ($appName)"]]);
 
       // check if request was successful
       if ($request->getStatusCode() != 200) {
         CRM_Core_Error::debug_log_message('Geocoding failed, invalid response code ' . $request->getStatusCode());
+        return ['geo_code_error' => 'Geocoding failed, invalid response code ' . $request->getStatusCode()];
         if ($request->getStatusCode() == 429) {
           // provider says 'TOO MANY REQUESTS'
-          $values['geo_code_error'] = 'OVER_QUERY_LIMIT';
-        } else {
-          $values['geo_code_error'] = $request->getStatusCode();
+          return ['geo_code_error' => 'OVER_QUERY_LIMIT'];
         }
-        return FALSE;
+        else {
+          return ['geo_code_error' => $request->getStatusCode()];
+        }
       }
 
       // Process results
@@ -176,30 +198,33 @@ class CRM_Utils_Geocode_OpenStreetMapCoding {
       // $string could not be decoded; maybe the service is down...
       // We don't save this in the cache.
       CRM_Core_Error::debug_log_message('Geocoding failed. "' . $string . '" is no valid json-code. (' . $url . ')');
-      return FALSE;
+      return ['geo_code_error' => 'Geocoding failed. "' . $string . '" is no valid json-code. (' . $url . ')'];
 
-    } elseif (count($json) == 0) {
+    }
+    elseif (count($json) == 0) {
       // Array is empty; address is probably invalid...
       // Error logging is disabled, because it potentially reveals address data to the log
       // CRM_Core_Error::debug_log_message('Geocoding failed.  No results for: ' . $url);
-      $values['geo_code_1'] = $values['geo_code_2'] = 'null';
       // Save in cache so we don't keep repeating the same failed query.
       $cache->set($cacheKey, $json);
-      return FALSE;
-
-    } elseif (array_key_exists('lat', $json[0]) && array_key_exists('lon', $json[0])) {
+      return [];
+    }
+    elseif (array_key_exists('lat', $json[0]) && array_key_exists('lon', $json[0])) {
       // TODO: Process other relevant data to update address
-      $values['geo_code_1'] = substr($json[0]->lat, 0, 12);
-      $values['geo_code_2'] = substr($json[0]->lon, 0, 12);
       // Save in cache.
       $cache->set($cacheKey, $json);
-      return TRUE;
+      return [
+        'geo_code_1' => (float) substr($json[0]->lat, 0, 12),
+        'geo_code_2' => (float) substr($json[0]->lon, 0, 12),
+      ];
 
-    } else {
+    }
+    else {
       // Don't know what went wrong... we got an array, but without lat and lon.
       // We don't save this in the cache.
       CRM_Core_Error::debug_log_message('Geocoding failed. Response was positive, but no coordinates were delivered.');
-      return FALSE;
+      return [];
     }
   }
+
 }
