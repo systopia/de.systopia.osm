15cc78deaed90bb8c6f08ea608f075f6e11070a0
B. Endres <endres@systopia.de>
2022-12-06 10:56:15 +0100

    [#29] running civix upgrade

--- osm.php
+++ osm.php
@@ -3,7 +3,7 @@
   +--------------------------------------------------------------------+
   | CiviCRM OSM Geocoding module (SYS-OSM)                             |
   +--------------------------------------------------------------------+
-  | Copyright SYSTOPIA (c) 2014-2015                                   |
+  | Copyright SYSTOPIA (c) 2014-2022                                   |
   +--------------------------------------------------------------------+
   | This is free software; you can copy, modify, and distribute it     |
   | under the terms of the GNU Affero General Public License           |
@@ -33,33 +33,24 @@ function osm_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- */
-function osm_civicrm_xmlMenu(&$files) {
-  _osm_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  */
 function osm_civicrm_install() {
-  return _osm_civix_civicrm_install();
+  _osm_civix_civicrm_install();
 }
 
 /**
  * Implementation of hook_civicrm_uninstall
  */
 function osm_civicrm_uninstall() {
-  return _osm_civix_civicrm_uninstall();
+  _osm_civix_civicrm_uninstall();
 }
 
 /**
  * Implementation of hook_civicrm_enable
  */
 function osm_civicrm_enable() {
-  return _osm_civix_civicrm_enable();
+  _osm_civix_civicrm_enable();
 }
 
 /**
@@ -83,11 +74,19 @@ function osm_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implementation of hook_civicrm_managed
+ * Implements hook_civicrm_postInstall().
  *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
  */
-function osm_civicrm_managed(&$entities) {
-  return _osm_civix_civicrm_managed($entities);
+function osm_civicrm_postInstall() {
+  _osm_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function osm_civicrm_entityTypes(&$entityTypes) {
+  _osm_civix_civicrm_entityTypes($entityTypes);
 }
